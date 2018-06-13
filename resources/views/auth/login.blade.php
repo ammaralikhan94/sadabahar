32b3686b634ba6c993fb8a2116c0b44d791b2c17
Mujtaba Kamal <39509502+mujtabakamal12@users.noreply.github.com>
2018-06-13 13:53:20 +0500

    login changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,10 +31,10 @@
 
         <div class="account-pages"></div>
         <div class="clearfix"></div>
-        <div class="wrapper-page">
+        <div class="wrapper-page login">
             <div class=" card-box">
             <div class="panel-heading"> 
-                <h3 class="text-center"> Sign In to <strong class="text-custom">Sadabahar</strong> </h3>
+                <h3 class="text-center text-white"> Sign In to <strong class="text-custom">Sadabahar</strong> </h3>
             </div> 
 
 
@@ -62,7 +62,7 @@
                     <div class="col-xs-12">
                         <div class="checkbox checkbox-primary">
                             <input id="checkbox-signup" type="checkbox">
-                            <label for="checkbox-signup">
+                            <label for="checkbox-signup text-white">
                                 Remember me
                             </label>
                         </div>
@@ -78,19 +78,19 @@
 
                 <div class="form-group m-t-30 m-b-0">
                     <div class="col-sm-12">
-                        <a href="page-recoverpw.html" class="text-dark"><i class="fa fa-lock m-r-5"></i> Forgot your password?</a>
+                        <a href="page-recoverpw.html" class="text-white forgot-pass"><i class="fa fa-lock m-r-5"></i> Forgot your password?</a>
                     </div>
                 </div>
             </form> 
             
             </div>   
             </div>                              
-                <!-- <div class="row">
-                <div class="col-sm-12 text-center">
-                    <p>Don't have an account? <a href="page-register.html" class="text-primary m-l-5"><b>Sign Up</b></a></p>
-                        
-                    </div>
-            </div> -->
+            <!-- <div class="row">
+            <div class="col-sm-12 text-center">
+                <p>Don't have an account? <a href="page-register.html" class="text-primary m-l-5"><b>Sign Up</b></a></p>
+                    
+                </div>
+        </div> -->
             
         </div>
         
